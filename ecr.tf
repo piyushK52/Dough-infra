c1a859ccbb866fbbfb2546d50bf4e142ac3a3229
Piyush Kumar <piyush.kumar.iaf@gmail.com>
2023-09-02 03:35:07 +0530

    deployment permissions added for pipeline user

--- ecr.tf
+++ ecr.tf
@@ -6,10 +6,40 @@ data "aws_iam_policy_document" "ecr_policy" {
   }
 }
 
+# allowing pipeline to register new task
+data "aws_iam_policy_document" "ecs_policy" {
+  statement {
+    actions   = ["ecs:RegisterTaskDefinition", "ecs:DescribeServices", "ecs:UpdateService"]
+    resources = ["*"]
+  }
+}
+
+data "aws_iam_policy_document" "pass_role_policy" {
+  statement {
+    actions   = ["iam:GetRole", "iam:PassRole"]
+    resources = [
+      aws_iam_role.ecs_task_execution_role.arn,
+      aws_iam_role.ecs_task_role.arn
+    ]
+  }
+}
+
+resource "aws_iam_policy" "pass_role_policy" {
+  name        = "PassEcsTaskRolePolicy"
+  description = "Allows passing the ecs-task-role to ECS tasks."
+  policy = data.aws_iam_policy_document.pass_role_policy.json
+}
+
 resource "aws_iam_policy" "ecr_policy" {
   name        = "ecr-policy"
   description = "Custom ECR policy"
   policy      = data.aws_iam_policy_document.ecr_policy.json
+}
+
+resource "aws_iam_policy" "ecs_policy" {
+  name        = "ecs-policy"
+  description = "Custom ECS policy"
+  policy      = data.aws_iam_policy_document.ecs_policy.json
 }
 
 resource "aws_iam_user" "ecr_user" {
@@ -24,6 +54,17 @@ resource "aws_iam_policy_attachment" "attach_ecr_policy" {
   name       = "attach-ecr-policy"
   policy_arn = aws_iam_policy.ecr_policy.arn
   users      = [aws_iam_user.ecr_user.name]
+}
+
+resource "aws_iam_policy_attachment" "attach_ecs_policy" {
+  name       = "attach-ecs-policy"
+  policy_arn = aws_iam_policy.ecs_policy.arn
+  users      = [aws_iam_user.ecr_user.name]
+}
+
+resource "aws_iam_user_policy_attachment" "attach_pass_role_policy" {
+  user       = aws_iam_user.ecr_user.name
+  policy_arn = aws_iam_policy.pass_role_policy.arn
 }
 
 # access keys
