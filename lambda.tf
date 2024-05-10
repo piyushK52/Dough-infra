4e867f6646c3fd9c2caac4f04c5bb9d7170b5894
piyushk52 <piyush.kumar.iaf@gmail.com>
2024-05-10 08:37:49 +0530

    generate_collage and generate_zip lambdas added

--- lambda.tf
+++ lambda.tf
@@ -50,6 +50,20 @@ resource "aws_iam_role_policy_attachment" "lambda_general_s3_temp_attachment" {
   policy_arn = aws_iam_policy.lambda_temp_s3_policy.arn
 }
 
+# defining layer variables -------------------------------------
+
+# NOTE: some dude has maintained these layers in different regions for different python versions - https://api.klayers.cloud/api/v2/p3.10/layers/latest/ap-south-1/html
+# TODO: self-host the layers 
+variable "pillow_layer_arn" {
+  default = "arn:aws:lambda:ap-south-1:770693421928:layer:Klayers-p310-Pillow:7"
+}
+
+variable "requests_layer_arn" {
+  default = "arn:aws:lambda:ap-south-1:770693421928:layer:Klayers-p310-requests:10"
+}
+
+# defining lambda functions ---------------------------------------
+
 resource "aws_lambda_function" "cleanup_cron" {
   filename      = "./lambda/lambda_cleanup_cron.zip"
   function_name = "lambda_cleanup_cron"
@@ -63,6 +77,58 @@ resource "aws_lambda_function" "cleanup_cron" {
 
   memory_size = 128
   timeout = 100
+
+  depends_on = [
+    aws_iam_role_policy_attachment.lambda_general_policy_attachment,
+    aws_iam_role_policy_attachment.lambda_general_s3_temp_attachment
+  ]
+}
+
+
+resource "aws_lambda_function" "generate_collage" {
+  filename      = "./lambda/generate_collage.zip"
+  function_name = "generate_collage"
+  role          = aws_iam_role.lambda_iam_assume_role.arn
+  handler       = "lambda_handler.generate_collage"
+  publish       = true
+
+  source_code_hash = filebase64sha256("./lambda/generate_collage.zip")
+
+  runtime = "python3.10"
+
+  memory_size = 256
+  timeout = 100
+
+  layers = [
+    var.pillow_layer_arn,
+    var.requests_layer_arn
+  ]
+
+  depends_on = [
+    aws_iam_role_policy_attachment.lambda_general_policy_attachment,
+    aws_iam_role_policy_attachment.lambda_general_s3_temp_attachment
+  ]
+}
+
+
+resource "aws_lambda_function" "generate_zip" {
+  filename      = "./lambda/generate_zip.zip"
+  function_name = "generate_zip"
+  role          = aws_iam_role.lambda_iam_assume_role.arn
+  handler       = "lambda_handler.generate_zip"
+  publish       = true
+
+  source_code_hash = filebase64sha256("./lambda/generate_zip.zip")
+
+  runtime = "python3.10"
+
+  memory_size = 256
+  timeout = 100
+
+  layers = [
+    var.pillow_layer_arn,
+    var.requests_layer_arn
+  ]
 
   depends_on = [
     aws_iam_role_policy_attachment.lambda_general_policy_attachment,
